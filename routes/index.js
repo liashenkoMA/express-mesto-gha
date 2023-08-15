53fc94e4996fedaa0435fd5b43fd3e9d18a2e8a3
Maksim Liashenko <maliashenko@yandex.ru>
2023-08-15 17:36:02 +0400

    fix: ошибки гита 3

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.patch('*', (req, res) => { res.status(404).send({ messange: 'Страница не найдена' }); });
 
 module.exports = router;
