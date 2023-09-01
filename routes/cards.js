bc86bd4861f7789ee71b9eb4f374b552b33e3d5a
Maksim Liashenko <maliashenko@yandex.ru>
2023-09-01 19:20:02 +0400

    fix: 2

--- routes/cards.js
+++ routes/cards.js
@@ -4,50 +4,31 @@ const {
 } = require('../controllers/cards');
 const { celebrate, Joi } = require('celebrate');
 
-router.get('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
-    owner: Joi.object(),
-    likes: Joi.object(),
-    createAt: Joi.date(),
-  }).unknown(true),
-}), getAllCards);
+router.get('/', getAllCards);
 router.delete('/:cardId', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
-    owner: Joi.object(),
-    likes: Joi.object(),
-    createAt: Joi.date(),
-  }).unknown(true),
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
 }), deleteCard);
+
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
+    link: Joi.string().required().uri().pattern(/^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/),
     owner: Joi.object(),
     likes: Joi.object(),
     createAt: Joi.date(),
   }).unknown(true),
 }), createCard);
 router.put('/:cardId/likes', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
-    owner: Joi.object(),
-    likes: Joi.object(),
-    createAt: Joi.date(),
-  }).unknown(true),
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
 }), putLikeCard);
 router.delete('/:cardId/likes', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
-    owner: Joi.object(),
-    likes: Joi.object(),
-    createAt: Joi.date(),
-  }).unknown(true),
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
 }), deleteLikeCard);
 
 module.exports = router;
